7e0ed5040ffbae3523557cbf502584f1a103bc45
AgapeCoin4all Ltd. <agapecoin4all@gmail.com>
2025-10-14 23:53:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3996904761-5d410",
+  "appId": "1:508732961445:web:0ba671b3b93538739a934c",
+  "apiKey": "AIzaSyDLnL6HXnCo4ff8sYQ7qM06b9mACLg5UsQ",
+  "authDomain": "studio-3996904761-5d410.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "508732961445"
+};
